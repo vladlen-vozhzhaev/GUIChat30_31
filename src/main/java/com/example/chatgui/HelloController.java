b53f6731cbbfc2c09b9b77b3d89d371561625185
Vladlen <vladlen@vozhzhaev.ru>
2022-08-02 21:23:48 +0300

    Реализовать вывод сообщений из БД

--- src/main/java/com/example/chatgui/HelloController.java
+++ src/main/java/com/example/chatgui/HelloController.java
@@ -4,15 +4,15 @@ import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.Socket;
 import org.json.simple.*;
 import org.json.simple.parser.JSONParser;
 
 public class HelloController {
     DataOutputStream out;
+    DataInputStream is;
+    Socket socket;
     @FXML
     TextField textField;
     @FXML
@@ -26,11 +26,29 @@ public class HelloController {
 
     }
     @FXML
-    protected void auth(){
-        if(login == null)
-            textArea.appendText("Введите логин\n");
-        else if(pass == null)
-            textArea.appendText("Введите пароль\n");
+    protected void auth() throws IOException {
+        String token = "";
+        try {
+            FileInputStream fis = new FileInputStream("C://java/token.txt");
+            int i = -1;
+            while ((i = fis.read()) != -1){
+                token += ((char) i);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        if (token.equals("")){
+            if(login == null)
+                textArea.appendText("Введите логин\n");
+            else if(pass == null)
+                textArea.appendText("Введите пароль\n");
+        }else{
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("token", token);
+            jsonObject.put("login", "");
+            jsonObject.put("pass", "");
+            out.writeUTF(jsonObject.toJSONString());
+        }
     }
     @FXML
     protected void handlerSand() throws IOException {
@@ -49,6 +67,7 @@ public class HelloController {
                 JSONObject jsonObject = new JSONObject();
                 jsonObject.put("login", login);
                 jsonObject.put("pass", pass);
+                jsonObject.put("token", "");
                 out.writeUTF(jsonObject.toJSONString());
                 login = null;
                 pass = null;
@@ -58,9 +77,9 @@ public class HelloController {
     @FXML
     public void connect(){
         try {
-            Socket socket = new Socket("127.0.0.1", 9743);
-            this.out = new DataOutputStream(socket.getOutputStream());
-            DataInputStream is = new DataInputStream(socket.getInputStream());
+            this.socket = new Socket("127.0.0.1", 9743);
+            this.out = new DataOutputStream(this.socket.getOutputStream());
+            this.is = new DataInputStream(this.socket.getInputStream());
             Thread thread = new Thread(new Runnable() {
                 @Override
                 public void run() {
@@ -70,10 +89,19 @@ public class HelloController {
                             String response = is.readUTF();
                             JSONParser jsonParser = new JSONParser();
                             JSONObject jsonResponse = (JSONObject) jsonParser.parse(response);
+                            System.out.println(jsonResponse.get("authResult"));
                             if(jsonResponse.get("authResult")!= null){
+
                                 if(jsonResponse.get("authResult").equals("error"))
                                     textArea.appendText("Неправильный логин или пароль\n");
-                                else if(jsonResponse.get("authResult").equals("success")) isAuth = true;
+                                else if(jsonResponse.get("authResult").equals("success")) {
+                                    isAuth = true;
+                                    String token = jsonResponse.get("token").toString();
+                                    FileOutputStream fos = new FileOutputStream("C://java/token.txt");
+                                    byte[] buffer = token.getBytes();
+                                    fos.write(buffer);
+                                    fos.close();
+                                }
                             } else if(jsonResponse.get("users") != null){
                                 JSONArray jsonArray = (JSONArray) jsonParser.parse(jsonResponse.get("users").toString());
                                 for (int i = 0; i < jsonArray.size(); i++) {
